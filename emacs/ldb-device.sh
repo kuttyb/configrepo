fae8b92ae525f5d257bf0af25ef07eb366fed1bf
Kutty Banerjee <kutty.banerjee@gmail.com>
2015-10-03 17:52:02 -0700

    changes

--- emacs/ldb-device.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-debugserver localhost:2345 -- $1
--- emacs/ldb.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# $1 = binary
-
-#xcrun -sdk iphoneos9.0.internal lldb --arch arm64 $1
-#xcrun -sdk iphoneos8.1.internal lldb --arch arm64 $1
-xcrun -sdk $CURR_SDKROOT  lldb --arch arm64 $1
